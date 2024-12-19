1d08dfd783b5056deb9edc7a9c82fc3e87251afa
WIN-96OONA5LS27\wldud <jy8408@gmail.com>
2024-12-19 10:51:19 +0900

    project

---
+++ src/main/java/com/acorn/awsHello/AwsHelloApplication.java
@@ -0,0 +1,13 @@
+package com.acorn.awsHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public claAwsHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsHelloApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/acorn/awsHello/HelloController.java
@@ -0,0 +1,13 @@
+package com.acorn.awsHello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hiview";
+    }
+}
